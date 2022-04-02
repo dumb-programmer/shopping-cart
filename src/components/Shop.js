4effdb072575e918ba557dc6380d4d855605400d
dumb-programmer <asadkhan6192@gmail.com>
2022-04-02 23:08:46 +0500

    Add some items to shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import "../styles/Shop.css";
+
 const Shop = () => {
+  const [items, setItems] = useState([
+    { id: 0, name: "Item 1", price: "10$", qty: 1 },
+    { id: 1, name: "Item 2", price: "15$", qty: 1 },
+    { id: 2, name: "Item 3", price: "25$", qty: 1 },
+    { id: 3, name: "Item 4", price: "50$", qty: 1 },
+    { id: 4, name: "Item 5", price: "30$", qty: 1 },
+  ]);
   return (
-    <div>
-      <h1>Shop Page</h1>
+    <div className="card-container">
+      {items.map((item) => (
+        <div className="card">
+          <img src="https://via.placeholder.com/215x160" alt="placeholder" />
+          <h1>{item.name}</h1>
+        </div>
+      ))}
     </div>
   );
 };
