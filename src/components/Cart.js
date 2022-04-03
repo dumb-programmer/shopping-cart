7da54631e2038279c6d0ed2ae25a543150581369
dumb-programmer <asadkhan6192@gmail.com>
2022-04-03 22:48:33 +0500

    Implement basic cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,24 @@
+import { useOutletContext } from "react-router-dom";
+
 const Cart = () => {
+  const [cart, setCart] = useOutletContext();
+  function calculateTotal(items) {
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+      total += items[i].price * items[i].qty;
+    }
+    return total;
+  }
   return (
-    <div>
-      <h1>Cart Page</h1>
+    <div className="cart-container">
+      <ul>
+        {cart.map((item) => (
+          <p key={item.id}>
+            {item.name} - {item.price}$ - {item.qty}
+          </p>
+        ))}
+      </ul>
+      <h3>Total : {calculateTotal(cart)}$</h3>
     </div>
   );
 };
