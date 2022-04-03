eadabeb6acca858af541d66c23d0d2d6f847241f
dumb-programmer <asadkhan6192@gmail.com>
2022-04-03 12:24:12 +0500

    Add price info and add to cart button to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import "../styles/Card.css";
 
-const Card = ({ name }) => {
+const Card = ({ name, price, index, onBtnClick }) => {
   return (
-    <div className="card">
-      <img src="https://via.placeholder.com/215x160" alt="placeholder" />
+    <div className="card" data-index={index}>
+      <img src="https://via.placeholder.com/225x160" alt="placeholder" />
       <h1>{name}</h1>
+      <p>{price}</p>
+      <button onClick={onBtnClick}>Add to cart</button>
     </div>
   );
 };
