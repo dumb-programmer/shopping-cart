c5c5bb83837b41cfc41350e5bea39f94e9e4bb66
dumb-programmer <asadkhan6192@gmail.com>
2022-04-03 22:48:16 +0500

    Make price an integer instead of string and use $ symbol in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, price, index, onBtnClick }) => {
     <div className="card" data-index={index}>
       <img src="https://via.placeholder.com/225x160" alt="placeholder" />
       <h1>{name}</h1>
-      <p>{price}</p>
+      <p>{price}$</p>
       <button onClick={onBtnClick}>Add to cart</button>
     </div>
   );
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,11 +5,11 @@ import "../styles/Shop.css";
 
 const Shop = () => {
   const [items, setItems] = useState([
-    { id: 0, name: "Item 1", price: "10$", qty: 1 },
-    { id: 1, name: "Item 2", price: "15$", qty: 1 },
-    { id: 2, name: "Item 3", price: "25$", qty: 1 },
-    { id: 3, name: "Item 4", price: "50$", qty: 1 },
-    { id: 4, name: "Item 5", price: "30$", qty: 1 },
+    { id: 0, name: "Item 1", price: 10, qty: 1 },
+    { id: 1, name: "Item 2", price: 15, qty: 1 },
+    { id: 2, name: "Item 3", price: 25, qty: 1 },
+    { id: 3, name: "Item 4", price: 50, qty: 1 },
+    { id: 4, name: "Item 5", price: 30, qty: 1 },
   ]);
 
   const [cart, setCart, cartCount, setCartCount] = useOutletContext();
