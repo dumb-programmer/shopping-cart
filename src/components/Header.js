5d89b1675dfda3166416d27cf30a012de4e2301a
dumb-programmer <asadkhan6192@gmail.com>
2022-04-07 22:50:06 +0500

    Add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,37 @@
 import { Link } from "react-router-dom";
 import cartIcon from "../assests/icons/shopping-cart.svg";
+import bookIcon from "../assests/icons/book-open.svg";
 import "../styles/Header.css";
 
 const Header = ({ cartCount }) => {
   return (
     <nav>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="shop">Shop</Link>
-      </li>
-      <li>
-        <Link to="cart">
-          <img src={cartIcon} alt="cart icon" />
+      <div className="logo">
+        <Link to="/">
+          <img src={bookIcon} alt="logo" />
+          <span>The Book Shop</span>
         </Link>
-        {cartCount ? (
-          <sup>
-            <span className="cart-length">{cartCount}</span>
-          </sup>
-        ) : (
-          ""
-        )}
-      </li>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="cart">
+            <img src={cartIcon} alt="cart icon" />
+          </Link>
+          {cartCount ? (
+            <sup>
+              <span className="cart-length">{cartCount}</span>
+            </sup>
+          ) : (
+            ""
+          )}
+        </li>
+      </ul>
     </nav>
   );
 };
